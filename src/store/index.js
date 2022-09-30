5ce917d3e2e5f94972023f5e713f29ad246ea32c
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-30 22:13:34 +0300

    redux persist

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+
+import loginSlice from "./loginSlice";
+
+const reducers = combineReducers({
+  login: loginSlice.reducer,
+});
+
+const rootReducer = (state, action) => {   
+  // Clear all data in redux store to initial.
+  if(action.type === "DESTROY_SESSION"){
+    localStorage.removeItem('persist:root');
+    state = undefined;
+  }
+     
+  
+  return reducers(state, action);
+};
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["ui"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export default store;
