f65122ec7b2d2a799d74474a3dcd45cd7a909f79
davidkaggulire1 <dkaggulire@gmail.com>
2022-09-30 22:12:48 +0300

    enable login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { Suspense, useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
+import { loginActions } from "./store/loginSlice";
+import { calculateRemainingTime } from "./utils/retrieveToken";
 
 const WelcomePage = React.lazy(() => import("./components/Welcome"));
 const DashboardPage = React.lazy(() =>
@@ -14,15 +17,49 @@ const VehiclePage = React.lazy(() =>
   import("./components/Dashboard/VehiclesList")
 );
 
+const NotFound = React.lazy(() =>
+  import("./components/NotFound")
+);
+let logoutTimer;
+
 function App() {
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+  const navigate = useNavigate();
+
+  const expirationTime = useSelector((state) => state.login.expirationTime);
+  const dispatch = useDispatch();
+
+  const logoutHandler = useCallback(() => {
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const remainingTime = calculateRemainingTime(expirationTime);
+      dispatch(
+        loginActions.expire({
+          remainingTime: remainingTime,
+        })
+      );
+      console.log(remainingTime);
+      logoutTimer = setTimeout(logoutHandler, remainingTime);
+      if (remainingTime < 0) {
+        navigate("../", { replace: true });
+      }
+    }
+  }, [logoutHandler, isLoggedIn, expirationTime, dispatch, navigate])
+
   return (
     <Suspense>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/cartypes" element={<CartypesPage />} />
-        <Route path="/charges" element={<ChargesPage />} />
-        <Route path="/vehicles" element={<VehiclePage />} />
+        {isLoggedIn && <Route path="/dashboard" element={<DashboardPage />} />}
+        {isLoggedIn && <Route path="/cartypes" element={<CartypesPage />} />}
+        {isLoggedIn && <Route path="/charges" element={<ChargesPage />} />}
+        {isLoggedIn && <Route path="/vehicles" element={<VehiclePage />} />}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
   );
