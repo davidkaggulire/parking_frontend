0df3cde02d76473d485349a005e417725a50c35b
davidkaggulire1 <dkaggulire@gmail.com>
2023-04-09 01:41:52 +0300

    deleted files

--- src/components/Dashboard/Tables/charges/BodaChargesTable.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import GeneralTable from "../GeneralTable";
-import { COLUMNS } from "./ChargeColumns";
-
-const BodaChargesTable = (props) => {
-  return <GeneralTable columns={COLUMNS} data={props.data} />;
-};
-
-export default BodaChargesTable;
--- src/components/Dashboard/Tables/charges/CarChargeTable.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import GeneralTable from "../GeneralTable";
-import { COLUMNS } from "./ChargeColumns";
-
-const CarChargeTable = (props) => {
-  return <GeneralTable columns={COLUMNS} data={props.data} />;
-};
-
-export default CarChargeTable;
